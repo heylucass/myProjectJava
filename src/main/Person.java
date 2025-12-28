1d886b110d6f22a297bb696a4e337c274d147dca
heylucass <luccasnascimentos4@gmail.com>
2025-12-27 23:37:54 -0300

    nova classe Person

---
+++ src/main/Person.java
@@ -0,0 +1,51 @@
+package main;
+
+public class Person {
+	private String name;
+	private Integer age;
+	private String address;
+	private String email;
+	private String[] preferences;
+	private Boolean isOnline; 
+	
+	// Metodo Construtor
+	public Person(String name, Integer age, String address, String email) {
+		this.address = address;
+		this.age = age;
+		this.name = name;
+		this.email = email;
+	}
+	
+	// Metodos Getters e Setters
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
--- src/main/start.java
+++ src/main/start.java
@@ -2,46 +2,41 @@ package main;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class start {
 	
 	public static void main(String[] args) {
-		// Tipos Primitivos de Inteiros
-		byte bytes = 0110; // 8 bits
-		short curto = 01500; // 16 bits
-		int inteiro = 12502330; // 32 bits
-		long longo = 824212003; // 64 bits
 		
-		// Representar numeros de bases diferentes
-		int decimal = 81923; // Inicia com 1 a 9(Base 10)
-		int octal = 034200; // Inicia com 0 e os numeros seguintes devem ser entre 0 e 7(Base 8)
-		int hexadecimal = 0xFF; // Inicia com 0x e os os digitos a seguir são de 0 a 9 e de A a F;(Base 16)
-		int binario = 0b100; // Inicia com 0b e os numero a seguir são 0 e 1(Base 2)
-		
-		// Tipos Primitivos de ponto flutuante
-		float flutuante = 2.42F; // 32 bits
-		double dobro = 57.123; // 64 bits
-		
-		// Outros tipos primitivos
-		boolean booleano = true; // True ou False
-		char caractere = 's';
-		
-		// Tipos não-primitivos
-		String palavra = "Palavra";
-		int[] numeros = {5, 6, 7, 1, 0}; // Array de inteiros
-		String[] str = {"P","e","R","as"}; // Array de Strings
-		
-		// Classes Wrappers
-		Boolean bool = false;
-		Integer novoInteiro = 245012;
-		Short novoCurto = 12500;
-		Byte novoByte = 123;
-		
+		String nome = JOptionPane.showInputDialog("Qual seu nome");
+		System.out.println(nome);
 		
 		System.out.println("Hello World!");
 		Scanner scn = new Scanner(System.in);
-		System.out.println("Digite um numero:");
-		int number = scn.nextInt();
-		System.out.println("O numero digitado foi:" + number);
+		
+		System.out.println("Digite seu nome: ");
+		String name = scn.nextLine();
+		System.out.println(name);
+		
+		System.out.println("Digite sua idade: ");
+		Integer age = scn.nextInt();
+		System.out.println(age);
+		
+		scn.nextLine();
+		
+		System.out.println("Digite seu endereço: ");
+		String address = scn.nextLine();
+		System.out.println(address);
+			
+		System.out.println("Digite seu email: ");
+		String email = scn.nextLine();
+		
+		Person novaPessoa = new Person(name, age, address, email);
+		
+		System.out.println("Nova pessoa cadastrada: " + novaPessoa.getName());
+		System.out.println("Idade: " + novaPessoa.getAge());
+		System.out.println("Endereço: " + novaPessoa.getAddress());
+		System.out.println("Email: " + novaPessoa.getEmail());
 		scn.close();
 	}
 }
